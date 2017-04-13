cb8c7e1a821faa9b62ba3dd90e48f61dc27fedf6
Yevhenii <honcharenko.zhenya@gmail.com>
2017-04-13 22:26:17 +0300

    test

---
+++ my-projects/empeek/app.js
@@ -0,0 +1,105 @@
+var newsApp = angular.module('newsApp', []);
+
+newsApp.controller('NewsAppController', function NewsAppController($scope) {
+  // localStorage.clear()
+  // Begin data for news
+  var news = [
+    {
+      name: 'First item with custom name',
+      isActive: false,
+      comments: [
+        {
+          authorName: 'Lorem',
+          comment: 'Lorem ipsum dolor sit amet ' +
+          'consectetur adipisicing elit. Sint ducimus, ' +
+          'expedita quaerat maxime hic. Aliquam sit perspiciatis ' +
+          'debitis eveniet ipsum harum cum a nulla ullam, nam ' +
+          'repudiandae sequi adipisci architecto.'
+        },
+        {
+          authorName: 'Lorem2',
+          comment: 'Lorem2 ipsum dolor sit amet ' +
+          'consectetur adipisicing elit. Sint ducimus, ' +
+          'expedita quaerat maxime hic. Aliquam sit perspiciatis ' +
+          'debitis eveniet ipsum harum cum a nulla ullam, nam ' +
+          'repudiandae sequi adipisci architecto.'
+        },
+        {
+          authorName: 'Lorem3',
+          comment: 'Lorem3 ipsum dolor sit amet ' +
+          'consectetur adipisicing elit. Sint ducimus, ' +
+          'expedita quaerat maxime hic. Aliquam sit perspiciatis ' +
+          'debitis eveniet ipsum harum cum a nulla ullam, nam ' +
+          'repudiandae sequi adipisci architecto.'
+        }
+      ]
+    },
+    {
+      name: 'Second  item is active',
+      isActive: true,
+      comments: [
+        {
+          authorName: 'LoremE',
+          comment: 'LoremE ipsum dolor sit amet ' +
+          'consectetur adipisicing elit. Sint ducimus, ' +
+          'expedita quaerat maxime hic. Aliquam sit perspiciatis ' +
+          'debitis eveniet ipsum harum cum a nulla ullam, nam ' +
+          'repudiandae sequi adipisci architecto.'
+        }
+      ]
+    }
+  ];
+
+  // set localStorage
+  if (!localStorage.getItem('news')) _safeToLocalStorege(news);;
+
+
+  $scope.news = JSON.parse(localStorage.getItem('news'));
+
+  $scope.addNews = function () {
+
+    $scope.news.push({
+      name: $scope.newNews,
+      isActive: false,
+      comments: []
+    });
+
+    _safeToLocalStorege($scope.news);
+
+    $scope.newNews = null;
+  }
+
+  $scope.addComment = function (item, newComment) {
+    item.comments.push({
+      authorName: 'UserName',
+      comment: newComment
+    });
+
+    _safeToLocalStorege($scope.news);
+  }
+
+  $scope.setActive = function (item) {
+    _clearActiveNews();
+    item.isActive = true;
+    _safeToLocalStorege($scope.news);
+  }
+
+  $scope.deleteItem = function (index) {
+    console.log(index);
+    $scope.news.splice(index, 1);
+    _safeToLocalStorege($scope.news);
+  }
+
+  function _clearActiveNews() {
+    $scope.news = $scope.news.map(function (el) {
+      el.isActive = false;
+      return el;
+    })
+  }
+
+  function _safeToLocalStorege(news) {
+    localStorage.setItem('news', JSON.stringify(news));
+  }
+
+
+});
